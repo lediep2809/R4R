8bfca5fa9dc29eae6d56f7060e44935a356b9b53
Lê Điệp <35600474+lediep2809@users.noreply.github.com>
2023-04-12 21:35:28 +0700

    new

---
+++ R4R_API/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using R4R_API.Entity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add services to the container.
+builder.Services.AddDbContext<EF_DataContext>(
+                o => o.UseNpgsql(builder.Configuration.GetConnectionString("Ef_Postgres_Db"))
+            );
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
