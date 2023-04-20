dff9a2cf7afcc764d4b9a84cd905c51bcd06b663
Lê Điệp <35600474+lediep2809@users.noreply.github.com>
2023-04-21 00:27:38 +0700

    add db

--- R4R_API/Models/R4rContext.cs
+++ R4R_API/Models/R4rContext.cs
@@ -22,10 +22,9 @@ public partial class R4rContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=containers-us-west-12.railway.app;Database=railway;Username=postgres;Password=nwQ6SIdnBq9a3XcVh7IJ");
+    => optionsBuilder.UseNpgsql("Host=containers-us-west-12.railway.app;Database=railway;Username=postgres;Password=nwQ6SIdnBq9a3XcVh7IJ");
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Role>(entity =>
         {
