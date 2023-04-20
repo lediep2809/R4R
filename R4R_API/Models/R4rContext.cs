c86f7c3ca66d65caa5a5b61f110d247bb40e7675
Lê Điệp <35600474+lediep2809@users.noreply.github.com>
2023-04-21 01:04:18 +0700

    test

--- R4R_API/Models/R4rContext.cs
+++ R4R_API/Models/R4rContext.cs
@@ -21,8 +21,11 @@ public partial class R4rContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
+    private string host = Environment.GetEnvironmentVariable("PGHOST");
+
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    => optionsBuilder.UseNpgsql("Host=containers-us-west-12.railway.app;Port=7353;Database=railway;Username=postgres;Password=nwQ6SIdnBq9a3XcVh7IJ");
+    => optionsBuilder.UseNpgsql("Host="+ host + ";Port=7353;Database=railway;Username=postgres;Password=nwQ6SIdnBq9a3XcVh7IJ");
 
 protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
